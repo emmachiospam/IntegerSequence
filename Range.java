5581b021ca6cef4cdc9b165e8a3f2d5689a8436b
emmachio <emmany2409@gmail.com>
2020-12-07 21:29:39 -0500

    hasNext

--- Range.java
+++ Range.java
@@ -1,4 +1,28 @@
 import java.util.NoSuchElementException;
 public class Range implements IntegerSequence{
   private int start, end, current;
+
+  public Range(int start, int end) {
+    start = start;
+    current = start;
+    end = end;
+  }
+
+  public void reset() {
+    current = start;
+  }
+
+  public int length() {
+    int length = end - start + 1;
+    return length;
+  }
+
+  public boolean hasNext() throws NoSuchElementException {
+    if (current <= end) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
 }
