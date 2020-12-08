fb20b4799778cce88afd3737bd5bb36b18b3b27b
emmachio <emmany2409@gmail.com>
2020-12-08 12:38:05 -0500

    brought methods

--- ArraySequence.java
+++ ArraySequence.java
@@ -11,4 +11,31 @@ public class ArraySequence implements IntegerSequence{
     }
   }
 
+  public void reset() {
+    currentIndex = data[0];
+  }
+
+  public int length() {
+    int length = data.length;
+    return length;
+  }
+
+  public boolean hasNext() {
+    if (currentIndex < data.length) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+
+  public int next() throws NoSuchElementException{
+    if(hasNext()) {
+      currentIndex++;
+      return (currentIndex - 1);
+    }
+    else{
+      throw new NoSuchElementException("there is not such element");
+    }
+  }
 }
