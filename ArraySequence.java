4115463cb99d8ad8c83a641259b0c937b3b72893
emmachio <emmany2409@gmail.com>
2020-12-08 12:56:10 -0500

    done

--- ArraySequence.java
+++ ArraySequence.java
@@ -11,6 +11,15 @@ public class ArraySequence implements IntegerSequence{
     }
   }
 
+  public ArraySequence(IntegerSequence otherseq) {
+    data = new int[otherseq.length()];
+    otherseq.reset();
+    for(int i = 0; i < data.length; i++) {
+      data[i] = otherseq.next();
+    }
+    otherseq.reset();
+  }
+
   public void reset() {
     currentIndex = data[0];
   }
