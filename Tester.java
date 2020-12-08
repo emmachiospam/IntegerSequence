ed4fd5c581f8f0f4c819f36e6bbe3feef634fcce
emmachio <emmany2409@gmail.com>
2020-12-08 13:09:53 -0500

    test

--- Tester.java
+++ Tester.java
@@ -1,11 +1,14 @@
 public class Tester {
+
   public static void main(String[] args) {
     IntegerSequence r = new Range(10,20);
-      while(r.hasNext()){
-        System.out.print(r.next());
-         if( r.hasNext() )
-            System.out.print( ", " );
-      }
-      System.out.println();
+    IntegerSequence as = new ArraySequence(r);
+
+    System.out.println("ArraySequence(seq):");
+    while(as.hasNext()){
+      System.out.print(as.next()+", ");
+    }
+    System.out.println();
   }
+
 }
